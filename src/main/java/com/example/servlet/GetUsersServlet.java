b906417a9b72f3de8ef152a0705120cef21e2a89
Matvey Stetcenko <matveyvs1987@gmail.com>
2023-02-09 21:46:10 +0400

    Second try

--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,18 +1,20 @@
 package com.example.servlet;
 
+import com.example.User;
+import com.example.Warehouse;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-
+import java.util.Set;
 public class GetUsersServlet extends HttpServlet {
     public void init() {
-
     }
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        Set<User> userSet = Warehouse.getInstance().getUsers();
+        request.setAttribute("users", userSet);
         getServletContext().getRequestDispatcher("/jsp/users.jsp").forward(request,response);
     }
     public void destroy() {
