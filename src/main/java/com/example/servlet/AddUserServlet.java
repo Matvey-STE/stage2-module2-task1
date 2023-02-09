48424194292c9d54734548451db891fc05bd2f99
Matvey Stetcenko <matveyvs1987@gmail.com>
2023-02-09 19:24:52 +0400

    First try

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,35 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AddUserServlet extends HttpServlet {
+    public void init() {
+
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+
+        User getUser = new User(firstName,lastName);
+        Warehouse.getInstance().addUser(getUser);
+
+        request.setAttribute("users",Warehouse.getInstance().getUsers());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/jsp/users.jsp");
+        rd.forward(request,response);
+    }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        getServletContext().getRequestDispatcher("/jsp/add.jsp").forward(request,response);
+    }
+    public void destroy() {
+    }
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,20 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
-    //write your code here!
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class GetUsersServlet extends HttpServlet {
+    public void init() {
+
+    }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        getServletContext().getRequestDispatcher("/jsp/users.jsp").forward(request,response);
+    }
+    public void destroy() {
+    }
 }
